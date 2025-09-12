4ebda7cdb4791a7f5a241cedf8efcfd1c37f720b
Stanislav Snizhkovyi <stas.snezhkovoy@gmail.com>
2025-09-12 23:50:03 +0300

    initial monorepo (backend)

---
+++ packages/shared/modules/types.ts
@@ -0,0 +1,15 @@
+export interface SuperheroPreview {
+	id: string;
+	nickname: string;
+	image: string;
+}
+
+export interface Superhero {
+	id: string;
+	nickname: string;
+	real_name: string;
+	origin_description: string;
+	superpowers: string;
+	catch_phrase: string;
+	images: string[];
+}
